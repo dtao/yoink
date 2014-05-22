0775b5c0b0ee93db30edeb2939d9079fbf594e31
Dan Tao <daniel.tao@gmail.com>
2014-05-21 22:29:13 -0700

    added query + limit options

--- gimme.js
+++ gimme.js
@@ -1,10 +1,22 @@
 var request = require('request');
 
-function prettifyJSON(json) {
-  return JSON.stringify(JSON.parse(json), null, 2);
+function prettyJSON(data) {
+  return JSON.stringify(data, null, 2);
 }
 
-module.exports = function gimme(url) {
+function applyQuery(data, query) {
+  return query.split('.').reduce(function(result, property) {
+    if (result instanceof Array) {
+      return result.map(function(element) {
+        return element[property];
+      });
+    } else {
+      return result[property];
+    }
+  }, data);
+}
+
+module.exports = function gimme(url, options) {
   request(url, function(error, response, body) {
     if (error) {
       console.error(error);
@@ -16,6 +28,16 @@ module.exports = function gimme(url) {
       return;
     }
 
-    console.log(prettifyJSON(body));
+    var data = JSON.parse(body);
+
+    if (options.query) {
+      data = applyQuery(data, options.query);
+    }
+
+    if (options.limit && data instanceof Array && options.limit < data.length) {
+      data = data.slice(0, Math.max(options.limit, 0));
+    }
+
+    console.log(prettyJSON(data));
   });
 };
